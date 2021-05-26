12728347a8d4a21c9216e911838beb8832b3b8d1
eartheekapat <z5281620@unsw.edu.au>
2021-05-26 12:14:38 +1000

    array and array_ex

---
+++ array.py
@@ -0,0 +1,11 @@
+print("Hello")
+
+array = [10, 3, 2, 5]
+
+min = array[0]
+
+for num in array:
+    if num < min:
+        min = num
+
+print(min)
---
+++ array_ex/array_ex_1_2.py
@@ -0,0 +1,8 @@
+import numpy as np
+#The original NumPy array
+new_arr=np.array([1, 2, 3, 4, 5])
+print("Original Array is :",new_arr)
+
+#reversing using flip() Method
+res_arr=np.flip(new_arr)
+print("Resultant Reversed Array:",res_arr)
